9e93dd00b053fac94020cc669e02f818f76cccd7
Anten Skrabec <cebarks@gmail.com>
2018-11-01 15:47:16 -0600

    adds EnigmaTest#test_it_decrypts_a_ciphertext_given_date_and_key

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -11,13 +11,24 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
-  def test_it_encrypts
+  def test_it_encrypts_a_message
+
+  end
+
+  def test_it_encrypts_a_message_given_key
 
   end
 
   def test_it_encrypts_a_message_given_date_and_key
     expected = { encryption: "keder ohulw", key: "02715", date: "040895" }
     actual = @enigma.encrypt("hello world", "02715", "040895")
+
+    assert_equal expected, actual
+  end
+
+  def test_it_decrypts_a_ciphertext_given_date_and_key
+    expected = { decryption: "hello world", key: "02715", date: "040895" }
+    actual = @enigma.decrypt("keder ohulw", "02715", "040895")
 
     assert_equal expected, actual
   end
