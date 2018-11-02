9e9d8cfc3b29ea57a5e4fe04ef4a135ad6008fbf
Anten Skrabec <cebarks@gmail.com>
2018-11-02 17:07:17 -0600

    implements Enigma#get_random_key

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,4 +2,14 @@ class Enigma
   CHAR_SET = (('a'..'z').to_a << ' ').freeze
 
   
+
+  def get_random_key
+    key = rand(0..99999).to_s
+
+    until key.length == 5
+      key.prepend('0')
+    end
+    
+    key
+  end
 end
