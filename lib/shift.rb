ffec68aba33221b5bc3e07d790d4da80aaa1d463
Anten Skrabec <cebarks@gmail.com>
2018-11-02 12:19:51 -0600

    variable name change in Shift#date_offset

--- lib/shift.rb
+++ lib/shift.rb
@@ -9,8 +9,8 @@ class Shift
 
   def date_offset
     date_num = @date.strftime('%d%m%y').to_i
-    date_num_string = (date_num**2).to_s
-    last_4 = date_num_string[date_num_string.length-4..date_num_string.length]
+    date_str = (date_num ** 2).to_s
+    last_4 = date_str[date_str.length - 4..date_str.length]
     last_4.split('').map(&:to_i)
   end
 
