549c56744c526fb4601d0034c9c1a5f20fb67cbd
Anten Skrabec <cebarks@gmail.com>
2018-11-02 12:40:55 -0600

    make sure message is downcase'd before being chunked

--- lib/message.rb
+++ lib/message.rb
@@ -2,13 +2,13 @@ class Message
   include Enumerable
 
   def initialize(message, plaintext)
-    @chunks = message.scan(/.{1,4}/)
+    @chunks = message.downcase.scan(/.{1,4}/)
     @plain = plaintext
   end
 
   def each(&block)
     @chunks.each do |chunk|
-      yield chunk
+      block.call(chunk)
     end
   end
 end
