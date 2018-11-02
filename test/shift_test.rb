093947dd5f92709c69d1975aa117cf8042866d12
Anten Skrabec <cebarks@gmail.com>
2018-11-02 09:46:01 -0600

    adds offset calculation tests to ShiftTest

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -20,4 +20,20 @@ class ShiftTest < Minitest::Test
   def test_it_has_a_key
     assert_equal @key, @shift.key
   end
+
+  def test_it_has_a_date_offset
+    assert_equal [4, 6, 5, 6], @shift.date_offset
+  end
+
+  def test_it_has_a_key_offset
+    assert_equal [12, 23, 34, 45], @shift.key_offset
+  end
+
+  def test_it_has_an_offset
+    assert_equal [16, 29, 39, 51], @shift.offset
+  end
+
+  def test_it_can_simplify_offset
+    assert_equal [16, 2, 12, 24], @shift.simplify_offset
+  end
 end
