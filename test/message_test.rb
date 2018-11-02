789b3d4da4ace3f92d133106ab8420d775208f16
Anten Skrabec <cebarks@gmail.com>
2018-11-02 09:49:57 -0600

    adds MessageTest

---
+++ test/message_test.rb
@@ -0,0 +1,34 @@
+require_relative 'test_helper'
+
+require './lib/message'
+
+class MessageTest < Minitest::Test
+  def setup
+    @plaintext = Message.new('hello world', false)
+    @ciphertext = Message.new('keder ohulw', true)
+  end
+
+  def test_it_exists
+    assert_instance_of Message, @plaintext
+    assert_instance_of Message, @ciphertext
+  end
+
+  def test_it_knows_if_its_plain
+    refute @ciphertext.plain?
+    assert @plaintext.plain?
+  end
+
+  def test_it_can_enumerate_chunks
+    actual = []
+
+    @plaintext.each do |chunk|
+      actual << chunk
+    end
+
+    assert_equal %w[hell o\ wo rld], actual
+  end
+
+  def test_it_has_chunks
+    assert_equal %w[hell o\ wo rld], @plaintext.chunks
+  end
+end
