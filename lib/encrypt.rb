fad5ca145b220f0fcd333be25d71fd3e113d2fee
Anten Skrabec <cebarks@gmail.com>
2018-11-06 11:48:18 -0700

    remove unneeded variable assignment

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,19 +2,17 @@
 require 'date'
 require_relative 'enigma'
 
-args = ARGV
-
-output_file = File.new(args[1], "w")
+output_file = File.new(ARGV[1], "w")
 
 enigma = Enigma.new
 date = enigma.format_date(Date.today)
 key = enigma.get_random_key
 
-File.foreach(args[0]) do |line|
+File.foreach(ARGV[0]) do |line|
   encrypted = enigma.encrypt(line, key, date)
   output_file.puts(encrypted[:encryption])
 end
 
-puts "Created '#{args[1]}' with the key #{key} and date #{date}"
+puts "Created '#{ARGV[1]}' with the key #{key} and date #{date}"
 
 output_file.close
